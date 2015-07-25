ee044fb94d1010cfc868448d3cb0f0712d27a533
R. BELLO <remi@evolya.fr>
2015-07-25 18:30:29 +0000

    Config

---
+++ config/config.default.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'connectionParams' => array(
+        'dbname'   => 'mydb',
+        'user'     => 'user',
+        'password' => 'secret',
+        'host'     => 'localhost',
+        'driver'   => 'pdo_mysql'
+    )
+);
