671aaddb0cd7881116562568b884b023a4082b16
R. BELLO <remi@evolya.fr>
2015-07-25 18:34:48 +0000

    Config

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
 
-echo 'Hello world from Cloud9!';
+error_reporting(E_ALL);
+
+require_once "vendor/autoload.php";
+
+include_once "config/load.php";
+
+$entityManager = EntityManager::create($conn, $config);
 
 ?>
