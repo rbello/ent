9f4eedf41796747712be102a22ec4315e4f273e1
R. BELLO <remi@evolya.fr>
2015-07-27 15:40:28 +0000

    Update

--- config/config.default.php
+++ config/config.default.php
@@ -1,11 +1,17 @@
 <?php
 return array(
-    'connectionParams' => array(
+    'persistence' => array(
         'dbname'   => 'mydb',
         'user'     => 'user',
         'password' => 'secret',
         'host'     => 'localhost',
         'driver'   => 'pdo_mysql'
+    ),
+    'auth0' => array(
+        'domain'        => 'Your domain',
+        'client_id'     => 'Your client ID',
+        'client_secret' => 'Your client Secret Key',
+        'redirect_uri'  => 'http://YOUR_APP/callback'
     ),
     'debug' => false
 );
--- index.php
+++ index.php
@@ -1,3 +1,33 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>title</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css" />
+    <script src="//code.angularjs.org/1.2.16/angular-cookies.min.js"></script>
+    <script src="//cdn.auth0.com/js/lock-7.5.min.js"></script>
+    <script src="//cdn.rawgit.com/auth0/angular-storage/master/dist/angular-storage.js"></script>
+    <script src="//cdn.rawgit.com/auth0/angular-jwt/master/dist/angular-jwt.js"></script>
+    <script src="//cdn.auth0.com/w2/auth0-angular-4.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+</head>
+<body>
+<script type="text/javascript">
+  
+  var lock = new Auth0Lock('Hs7HmVIHg8JnxpdLsLDQ3ZIRy5TPZAO0', 'ent.eu.auth0.com', {
+    assetsUrl: 'https://cdn.eu.auth0.com/'
+  });
 
-include 'system/init.php';
+  function signin() {
+    lock.show({
+        callbackURL: ''
+      , responseType: 'code'
+      , authParams: {
+        scope: 'openid profile'
+      }
+    });
+  }
+</script>
+<button onclick="signin()">Login</button>
+</body>
+</html>
