41f1c8c04f362502eaa7341df1df01292ad8673f
R. BELLO <remi@evolya.fr>
2015-08-10 00:22:55 +0000

    Test WWW

--- system/test/apis.php
+++ system/test/apis.php
@@ -20,3 +20,19 @@ if ($hash != '5977b94899dfcfe27672e124f2428176') {
 if (@!simplexml_load_string(file_get_contents('https://ent-rbello.c9.io/api/GestionSessions.wsdl'))) {
     echo "Error: WSDL xml validation\n";
 }
+
+if (@!file_get_contents('https://ent-rbello.c9.io/')) {
+    echo "Error: WWW /index.html\n";
+}
+
+if (@!file_get_contents('https://ent-rbello.c9.io/css/style.css')) {
+    echo "Error: WWW /css/style.css\n";
+}
+
+if (@!file_get_contents('https://ent-rbello.c9.io/res/google-map/google-map.html')) {
+    echo "Error: WWW /res/google-map/google-map.html\n";
+}
+
+if (@!file_get_contents('https://ent-rbello.c9.io/js/ctrl.js')) {
+    echo "Error: WWW /js/ctrl.js\n";
+}
